835329af67708deaf496f8ee1a4a940e2069c8a4
ke tan <147895238+tkzzzzzz6@users.noreply.github.com>
2024-12-22 08:48:13 +0000

    实现加一功能的解决方案，处理数字数组的进位情况

--- leetcode/66/1.cpp
+++ leetcode/66/1.cpp
@@ -1,1 +1,24 @@
-#include <s
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int len = digits.size();
+        // 从后往前遍历,如果当前位是9,则将当前位设置为0,继续遍历
+        for (int i = len - 1; i >= 0; i--)
+        {
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        // 如果所有位都是9,则在开头插入1
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
