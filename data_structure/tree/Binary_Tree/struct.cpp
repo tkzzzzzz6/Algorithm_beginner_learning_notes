af07209b7c8077e3518f4d78f417e48c327bd91a
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2024-12-31 21:33:32 +0800

    Refactor and clean up code formatting in Binary Tree and Disjoint Set implementations

--- data_structure/tree/Binary_Tree/struct.cpp
+++ data_structure/tree/Binary_Tree/struct.cpp
@@ -1,14 +1,10 @@
-typedef struct TNode *Position;
-typedef Position BinTree;
+typedef struct TNode *Position;
+typedef Position BinTree;
 
- /* 二叉树类型 */
-    struct TNode
-{
-     /* 树结点定义 */
-    ElementType Data;
-     /* 结点数据 */
-    BinTree Left;   
-         /* 指向左子树 */
-    BinTree Right;
-        /* 指向右子树 */
+/* 二叉树类型 */
+struct TNode {
+    /* 树结点定义 */
+    ElementType Data;    /* 结点数据 */
+    BinTree Left;       /* 指向左子树 */
+    BinTree Right;      /* 指向右子树 */
 };
