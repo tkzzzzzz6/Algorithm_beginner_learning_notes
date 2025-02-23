28f55aacc5b67f1195f3f848cd36741ba053cc82
Ke Tan <147895238+tkzzzzzz6@users.noreply.github.com>
2025-02-23 14:06:45 +0000

    [abc394] abc394_a

---
+++ abc394/abc394_a/abc394_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    for(int i = 0; i < n; i++){
+        if(s[i] == '2'){
+            cout << 2 ;
+        }
+    }
+    return 0;
+}
