ac50b9565be5d7949f98f0360dd6904d682e929f
tkzzzzzz6 <tkzzzzzz6@noreply.gitcode.com>
2025-04-11 21:10:24 +0800

    chore: 更新项目文件，优化代码结构
    - 移除了不再需要的示例代码和无用文件
    - 更新了文件组织，提升项目的可维护性和整洁性

--- cpp/base/base_loop/判断质数.cpp
+++ cpp/base/base_loop/判断质数.cpp
@@ -3,11 +3,18 @@
 #include <iomanip>
 #include <vector>
 
+// 多重循环的顺序可以类比时钟的顺序
 using namespace std;
 
 bool isPrime(int n)
 {
-    for (int i = 2; i * i <= n; ++i)
+    if (n <= 1)
+        return false;
+    if (n == 2)
+        return true;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i * i <= n; ++i)
     {
         if (n % i == 0)
             return false;
@@ -20,9 +27,7 @@ int main()
 {
     cin >> n;
     if (isPrime(n))
-    {
         cout << "Yes";
-    }
     else
     {
         printf("No");
