2216cef5de00e0aa372b9ccf876d834b44f6b37f
tkzzzzzz6 <tkzzzzzz6@noreply.gitcode.com>
2025-04-12 18:51:34 +0800

    chore: 清理项目文件，移除不再需要的示例代码和二进制文件
    - 删除了 3.cpp 文件，确保项目结构整洁
    - 移除了相关的二进制文件 3.exe

---
+++ cpp/base/stack_queue/十进制转N进制.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+// input:
+// 3467
+//
+// output:
+// 6613
+
+int main()
+{
+    int N, origin;
+    int digits[101];
+    int top = 0;
+    cout << "请输入对一个的进制和对应的数字" << endl;
+    cin >> N >> origin;
+    if (origin == 0)
+        cout << 0 << endl;
+    while (origin)
+    {
+        digits[++top] = origin % N;
+        origin /= N;
+    }
+    cout << "convert to N base numbers is ";
+    for (int i = 1; i <= top; ++i)
+    {
+        cout << digits[i];
+    }
+    cout << endl;
+
+    return 0;
+}
