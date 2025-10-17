b5e2299096555889ceb0b3468506883d7ad56053
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-10-17 12:13:36 +0800

    add niuke daily problem tk2

--- niuke/daily_problem/25_10-17最小循环节/1.cpp
+++ niuke/daily_problem/25_10-17最小循环节/1.cpp
@@ -5,10 +5,14 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-        
+    ios::sync_with_stdio(false), cin.tie(0);
 
+    string str;
+    cin >> str;
+
+    set<char> s(str.begin(), str.end());
+    cout << s.size() << '\n';
+    cout.flush();
 
     return 0;
 }
