a9a68072f7af2aef114737d76097662e6921f389
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-10-27 21:19:18 +0800

    Enhance SQL practice file with additional queries and remove obsolete file

--- niuke/practice/sql_practice/SQL快速入门/1.sql
+++ niuke/practice/sql_practice/SQL快速入门/1.sql
@@ -1,1 +1,20 @@
 select * from user_profile;
+
+select device_id,gender,age,university from user_profile;
+
+select distinct university from user_profile;
+
+select device_id from user_profile limit 2;
+
+select device_id as user_infos_example from user_profile limit 2;
+
+select device_id,university from user_profile where university = '北京大学';
+
+select device_id,gender,age,university from user_profile where age > 24;
+
+-- 8
+select device_id,gender,age from user_profile where age between 20 and 23;
+
+select device_id,gender,age,university from user_profile where university != '复旦大学';
+
+select device_id,gender,age,university from user_profile where age is not null;
--- niuke/practice/sql_practice/SQL快速入门/2.sql
+++
@@ -1,1 +0,0 @@
-select device_id,gender,age,university from user_profile;
