5142406939cb3a2e8f3e6864316098f30816c117
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-10-28 22:17:30 +0800

    Add additional SQL queries to enhance practice file, focusing on GPA and university filters

---
+++ niuke/daily_problem/25_10-28送分题/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+long long f(long long n)
+{
+    if (n < 20180001)
+        return n + 2017;
+    return f(f(n - 2018));
+}
+int main()
+{
+    long long n;
+    cin >> n;
+    if (n >= 20182017)
+        cout << 20182017;
+    else
+        cout << f(n) << endl;
+    return 0;
+}
--- niuke/practice/sql_practice/SQL快速入门/1.sql
+++ niuke/practice/sql_practice/SQL快速入门/1.sql
@@ -17,4 +17,14 @@ select device_id,gender,age from user_profile where age between 20 and 23;
 
 select device_id,gender,age,university from user_profile where university != '复旦大学';
 
+-- 10
 select device_id,gender,age,university from user_profile where age is not null;
+
+-- 11
+select device_id,gender,age,university,gpa from user_profile where gpa > 3.5 and gender = 'male';
+
+-- 12
+select device_id,gender,age,university,gpa from user_profile where university = "北京大学" or gpa > 3.7;
+
+-- 13
+select device_id,gender,age,university,gpa from user_profile where university in ('北京大学','复旦大学','山东大学');
