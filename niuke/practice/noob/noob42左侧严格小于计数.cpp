63ea6c6ff3539949561845728ac9dfec3b6b95c6
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-11-20 12:58:49 +0800

    Remove obsolete Python and C++ files from the noob practice directory to enhance project organization and maintainability.

--- niuke/practice/noob/noob42左侧严格小于计数.cpp
+++ niuke/practice/noob/noob42左侧严格小于计数.cpp
@@ -9,29 +9,35 @@
 typedef long long ll;
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false),cin.tie(0);
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(0);
     int t;
     cin >> t;
 
-    vector<int> a(t,0);
-    for(int i =0;i<t;++i)
+    vector<int> a(t, 0);
+    for (int i = 0; i < t; ++i)
     {
         cin >> a[i];
     }
     string ans = "";
-    for(int i = 0;i<t;++i)
+    for (int i = 0; i < t; ++i)
     {
-        for(int j = 0;j<i;++j)
+        int cnt = 0;
+        for (int j = 0; j < i; ++j)
         {
-            int cnt;
-            if(a[j] < a[i])
+            if (a[j] < a[i])
             {
                 ++cnt;
             }
-            ans += to_string(cnt) + ' ';
+        }
+        ans += to_string(cnt);
+        if(i != t-1)
+        {
+            ans += ' ';
         }
     }
+    cout << ans << endl;
 
     return 0;
 }
