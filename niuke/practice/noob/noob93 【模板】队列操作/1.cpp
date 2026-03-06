2ed3f9c09c0f445716aec9ac6013ad720cb80531
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-03-06 23:01:05 +0800

    重构队列类，修复前端访问错误并添加空检查

--- niuke/practice/noob/noob93 【模板】队列操作/1.cpp
+++ niuke/practice/noob/noob93 【模板】队列操作/1.cpp
@@ -3,14 +3,49 @@
 #include <vector>
 using namespace std;
 
+template <typename T>
+class Queue_e{
 
+    queue<T> q;
+public:
 
-template 
+    void push(const T& x){
+        q.push(x);
+    }
+
+    void pop(){
+        if(!q.empty()){
+            q.pop();
+        }else{
+          cout << "ERR_CANNOT_POP\n";
+        }
+    }
+
+    void front() const {
+        if(!q.empty()){
+            cout << q.fornt() << '\n';
+        }else{
+          cout << "ERR_CANNOT_QUERY\n";
+        }
+    }
+
+    void size() const {
+        cout << q.size() << '\n';
+    }
+
+    bool empty() const {
+        return q.empty();
+    }
+
+};
 
 int main() {
-    queue<int> q;
+    Queue_e<int> qe;
     int n;cin>> n;
 
+    while(n--){
+        
+    }
 
 }
 // 64 位输出请用 printf("%lld")
