3cd040e6df6708006248a916b7a96bfd5e7e274d
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-03-09 00:09:04 +0800

    新增连分数算法实现，包含两个主要函数的代码框架

---
+++ niuke/daily_problem/26-3-8连分数/2.cpp
@@ -0,0 +1,48 @@
+/*
+ * @Author: tkzzzzzz6
+ * @Date: 2026-03-09 00:02:35
+ * @LastEditors: tkzzzzzz6
+ * @LastEditTime: 2026-03-09 00:04:41
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+
+void murname(){
+    ll p,q,cnt1 = 0,cnt2 = 0;
+    cin >> p >> q;
+    cout << p << '/' << q << " = ";
+    vector<ll> ans;
+    while(p%q){
+        ans.push_back(p/q);
+        p = p % q;
+        swap(p,q);
+        cnt1++;cnt2++;
+    }ans.push_back(p/q);
+    if(cnt1 == 0){
+        cout << p / q;
+        return;
+    }cnt1--;
+    for(auto x:ans){
+        cout << x;
+        if(cnt1 >= 0)cout << "+1/";
+        if(cnt1 > 0)cout << '{';
+        cnt1--;
+    }while(cnt2-- > 1)cout << '}';
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        murname();
+        cout << '\n';
+    }
+    
+    return 0;
+}
