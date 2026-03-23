9c3c68d8fb0b94038301e86fac1410fab493c4de
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-03-23 14:06:28 +0800

    feat: 新增旋转图像算法实现，包含两种不同的旋转方法

--- leetcode/Study Plan/48. 旋转图像/1.cpp
+++ leetcode/Study Plan/48. 旋转图像/1.cpp
@@ -6,17 +6,17 @@
  */
 class Solution {
 public:
-  void rotate(vector<vector<int>> &matrix) {
-    int n = matrix.size();
-    // 转置
-    for (int i = 0; i < n; ++i) {
-      for (int j = 0; j < i; ++j) {
-        swap(matrix[i][j], matrix[j][i]);
-      }
-    }
+    void rotate(vector<vector<int>>& matrix) {
+       int n = matrix.size();
+        // 转置
+        for(int i = 0;i<n;++i){
+            for(int j = 0;j<i;++j){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
 
-    // 行翻转
-    for (auto &row : matrix)
-      ranges::reverse(row);
-  }
+        // 行翻转
+        for(auto &row:matrix)
+            ranges::reverse(row);
+    }
 };
