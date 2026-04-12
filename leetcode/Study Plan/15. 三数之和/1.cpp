088246603990b7b40ad33a2a5bcdbe7a6594eff9
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-04-12 15:21:39 +0800

    新增多种字符串和数组相关算法，包括三数之和、四数之和及反转字符串的实现

--- leetcode/Study Plan/15. 三数之和/1.cpp
+++ leetcode/Study Plan/15. 三数之和/1.cpp
@@ -2,22 +2,22 @@
  * @Author: tkzzzzzz6
  * @Date: 2026-04-12 13:35:46
  * @LastEditors: tkzzzzzz6
- * @LastEditTime: 2026-04-12 13:41:53
- */
-/*
- * @Author: tkzzzzzz6
- * @Date: 2026-04-12 13:35:46
- * @LastEditors: tkzzzzzz6
- * @LastEditTime: 2026-04-12 13:35:49
+ * @LastEditTime: 2026-04-12 13:48:47
  */
 class Solution {
 public:
   vector<vector<int>> threeSum(vector<int> &nums) {
     vector<vector<int>> result;
     int n = nums.size();
-    for (int i = 0; i < nums.size(); ++i) {
+    sort(nums.begin(), nums.end()); // 排序
+
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] > 0)
+        continue;
+
       if (i > 0 && nums[i] == nums[i - 1])
         continue;
+
       int left = i + 1, right = n - 1;
       while (left < right) {
         long long s = nums[i] + nums[left] + nums[right];
@@ -26,13 +26,17 @@ public:
         else if (s < 0)
           left++;
         else {
-          result.push({i, left, right});
-          while (nums[left] == nums[left + 1]) {
+          result.push_back(
+              {nums[i], nums[left], nums[right]}); // 存储数值而不是索引
+          while (left < right && nums[left] == nums[left + 1]) { // 添加边界检查
             left++;
           }
-          while (right < n - 1 && nums[right] == nums[right - 1]) {
-            right++;
+          while (left < right &&
+                 nums[right] == nums[right - 1]) { // 添加边界检查
+            right--; // 应该是 right--，不是 right++
           }
+          left++;  // 继续向右移动
+          right--; // 继续向左移动
         }
       }
     }
