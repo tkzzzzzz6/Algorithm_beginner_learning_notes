86f28ae860f80771d48e622328453084d24bbb5a
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-04-21 22:52:23 +0800

    feat: 添加 NP2 多行输出和 NP9 十六进制数字的大小的代码实现

---
+++ niuke/problems/编程入门-C/CPP1.定义变量.cpp
@@ -0,0 +1,35 @@
+/*
+ * @Author: tkzzzzzz6
+ * @Date: 2026-04-21 22:43:27
+ * @LastEditors: tkzzzzzz6
+ * @LastEditTime: 2026-04-21 22:43:48
+ */
+/**
+ * @nc app=nowcoder id=3ddbc410017a4da48f05a6c8619b32d2 topic=225 question=2175637 lang=C++
+ * 2026-04-21 22:43:27
+ * https://www.nowcoder.com/practice/3ddbc410017a4da48f05a6c8619b32d2?tpId=225&tqId=2175637
+ * [CPP1] 定义变量
+ */
+
+/** @nc code=start */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // write your code here......
+    char c;
+    int i;
+    long ll;
+    double d;
+
+    cout << sizeof(c) << endl;
+    cout << sizeof(i) << endl;
+    cout << sizeof(ll) << endl;
+    cout << sizeof(d) << endl;
+
+    return 0;
+}
+
+/** @nc code=end */
