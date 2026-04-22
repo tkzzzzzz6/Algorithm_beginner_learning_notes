675b440ef3390b7b61f8d06e607eeafd28c6f7d4
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-04-22 08:00:48 +0800

    feat: 添加二叉树的遍历实现及相关理论基础笔记

---
+++ leetcode/problems/94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ * @lcpr version=30204
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root == nullptr)return res;
+
+        inorderTraversal(root->left);
+        res.push_back(root->val);
+        inorderTraversal(root->right);
+
+        return res;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
