4ffae89da544794e02ed11264e763f9dbf3452f2
ke tan <202306630@stu.sicau.edu.cn>
2026-05-26 21:20:47 +0800

    refactor: 优化机器人项目管理代码结构和注释

---
+++ leetcode/problems/3120.count-the-number-of-special-characters-i.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=3120 lang=python3
+# @lcpr version=30204
+#
+# [3120] 统计特殊字母的数量 I
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        upper = 
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# "aaAbcBC"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "abc"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "abBCab"\n
+# @lcpr case=end
+
+#
