cc2fefd4f02ec3fdf2515afab47d70d0910bc316
tkzzzzzz6 <tk2535550189@gmail.com>
2026-05-26 21:17:18 +0800

    feat: 新增CSP201912A.报数.cpp文件，包含基本的C++程序结构

---
+++ CCF-CSP/CSP201912A. 报数.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Author: tkzzzzzz6
+ * @Date: 2026-05-26 13:58:16
+ * @LastEditors: tkzzzzzz6
+ * @LastEditTime: 2026-05-26 14:05:50
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
