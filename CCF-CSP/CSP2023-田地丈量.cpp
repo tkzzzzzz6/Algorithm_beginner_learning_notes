9341abe3eb68259b86c99024966e0136371ce6f7
tkzzzzzz6 <tk2535550189@gmail.com>
2026-05-28 13:01:39 +0800

    feat: 新增田地丈量算法的基础代码框架

---
+++ CCF-CSP/CSP2023-田地丈量.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: tkzzzzzz6
+ * @Date: 2026-05-28 08:06:15
+ * @LastEditors: tkzzzzzz6
+ * @LastEditTime: 2026-05-28 08:06:40
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
