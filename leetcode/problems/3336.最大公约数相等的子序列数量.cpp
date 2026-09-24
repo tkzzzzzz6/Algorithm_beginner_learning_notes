7a4d277a96c1b053b086a0a3e03ee3a90ec3790b
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-09-24 21:38:37 +0800

    添加医生问题的解决方案，包含状态压缩和子集枚举的实现；更新相关文档，删除不必要的逆元定义部分。

---
+++ leetcode/problems/3336.最大公约数相等的子序列数量.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=3336 lang=cpp
+ *
+ * [3336] 最大公约数相等的子序列数量
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int subsequencePairCount(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
--- niuke/daily_problem/26-7-13医生/1.cpp
+++ niuke/daily_problem/26-7-13医生/1.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int string2mask(const string& s) {
+int string2mask(const string &s) {
     int mask = 0;
 
     for (int i = 0; i < static_cast<int>(s.size()); ++i) {
@@ -21,7 +22,55 @@ int main() {
 
     int n, m;
     cin >> n >> m;
-    
 
-	return 0;
+    vector<int> patients(n);
+
+    for (int i = 0; i < n; ++i) {
+        string symptoms;
+        cin >> symptoms;
+        patients[i] = string2mask(symptoms);
+    }
+
+    int k;
+    cin >> k;
+
+    vector<int> medicines(k);
+
+    for (int i = 0; i < k; ++i) {
+        string symptoms;
+        cin >> symptoms;
+        medicines[i] = string2mask(symptoms);
+    }
+
+    int total = 1 << k;
+    vector<int> cover(total, 0);
+    vector<int> medicineCnt(total, 0);
+
+    for (int subset = 1; subset < total; ++subset) {
+        int lowbit = subset & -subset;
+        int previous = subset ^ lowbit;
+        int medicineIndex = __builtin_ctz(lowbit);
+
+        cover[subset] = cover[previous] | medicines[medicineIndex];
+        medicineCnt[subset] = medicineCnt[previous] + 1;
+    }
+
+    for (int patientMask : patients) {
+        int ans = k + 1;
+
+        for (int subset = 0; subset < total; ++subset) {
+            if (medicineCnt[subset] >= ans) continue;
+
+            if ((cover[subset] & patientMask) == patientMask) {
+                ans = medicineCnt[subset];
+            }
+        }
+
+        if (ans == k + 1)
+            cout << -1 << '\n';
+        else
+            cout << ans << '\n';
+    }
+
+    return 0;
 }
